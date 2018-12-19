d179b93e06c906c3eeb30859327a545824fa61a3
Alexios Karadimos <karadalex@gmail.com>
2018-12-19 14:49:38 +0200

    load FHIR json files to MongoDB

---
+++ src/load.py
@@ -0,0 +1,19 @@
+import os
+import json
+from pymongo import MongoClient
+
+
+client = MongoClient('mongodb://root:example@localhost:27017/')
+db = client.test_database
+fhir = db.fhir
+
+os.chdir('./data')
+for file in os.listdir(os.getcwd()):
+    if file.endswith(".json"):
+        with open(file) as json_data:
+            # Convert file to python object
+            resource = json.load(json_data)
+            resource_id = fhir.insert_one(resource).inserted_id
+            print(file + " " + str(resource_id))
+
+print("Loaded FHIR resources successfully")
